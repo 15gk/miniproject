6726cc3c615b7c0d8026e20fe6cf43e955df7421
Kriti <gautamkriti89@gmail.com>
2023-03-12 23:31:22 +0545

    added layout doe timer

---
+++ random_background_onclick/index1.js
@@ -0,0 +1,6 @@
+function changeColor(){
+    document.body.style.background =
+    'rgb('+Math.round(math.random()*255)+
+    ','+Math.round(math.random()*255)+','+Math.round(math.random()*255)+')';
+
+}
